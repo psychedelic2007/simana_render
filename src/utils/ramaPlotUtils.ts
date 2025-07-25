40c85bbeb4ae39af796b34c51ae5bda7e3401040
Satyam Sangeet <51230487+psychedelic2007@users.noreply.github.com>
2025-07-25 14:26:42 +1000

    Update ramaPlotUtils.ts

--- src/utils/ramaPlotUtils.ts
+++ src/utils/ramaPlotUtils.ts
@@ -33,7 +33,7 @@ export const generateRamachandranData = async (
       console.log(`${key}: ${value}`);
     }
     
-    const response = await fetch('http://localhost:8000/api/ramachandran/', {
+    const response = await fetch('https://simana.onrender.com/api/ramachandran/', {
       method: 'POST',
       body: formData,
     });
