704d6c3b83dbd5a0da36d4436498d57088fd63fa
Satyam Sangeet <51230487+psychedelic2007@users.noreply.github.com>
2025-07-25 17:35:27 +1000

    Update expandable-card-on-click.tsx

--- src/components/blocks/cards/expandable-card-on-click.tsx
+++ src/components/blocks/cards/expandable-card-on-click.tsx
@@ -55,7 +55,7 @@ export function ExpandableCardOnClick() {
   ];
   const [active, setActive] = useState<null | (typeof items)[number]>(null);
 
-  const ref = useRef<HTMLDivElement>(null);
+  const ref = useRef<HTMLDivElement | null>(null);
 
   useOutsideClick(ref, () => {
     setActive(null);
