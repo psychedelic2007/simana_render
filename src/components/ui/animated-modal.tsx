a5324c6e32a6bd91a2845ff8d890dfeccf9d575b
Satyam Sangeet <51230487+psychedelic2007@users.noreply.github.com>
2025-07-26 20:10:17 +1000

    Update animated-modal.tsx

--- src/components/ui/animated-modal.tsx
+++ src/components/ui/animated-modal.tsx
@@ -77,7 +77,7 @@ export const ModalBody = ({
     }
   }, [open]);
 
-  const modalRef = useRef(null);
+  const modalRef = useRef<HTMLDivElement>(null);
   const { setOpen } = useModal();
   useOutsideClick(modalRef, () => setOpen(false));
 
