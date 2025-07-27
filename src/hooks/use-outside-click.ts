832ef6082221877bae9240e4a5e5e9e791dbbc28
Satyam Sangeet <51230487+psychedelic2007@users.noreply.github.com>
2025-07-27 10:46:56 +1000

    Create use-outside-click.ts

---
+++ src/hooks/use-outside-click.ts
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+
+export const useOutsideClick = (
+  ref: React.RefObject<HTMLElement | null>,
+  callback: () => void
+) => {
+  useEffect(() => {
+    const listener = (event: any) => {
+      // Do nothing if clicking ref's element or descendent elements
+      if (!ref.current || ref.current.contains(event.target)) {
+        return;
+      }
+      callback();
+    };
+
+    document.addEventListener("mousedown", listener);
+    document.addEventListener("touchstart", listener);
+
+    return () => {
+      document.removeEventListener("mousedown", listener);
+      document.removeEventListener("touchstart", listener);
+    };
+  }, [ref, callback]);
+};
