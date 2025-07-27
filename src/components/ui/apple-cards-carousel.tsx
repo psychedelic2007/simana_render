ed77272cc8d1f82e876b025d6859dac3a79840b5
Satyam Sangeet <51230487+psychedelic2007@users.noreply.github.com>
2025-07-27 10:54:07 +1000

    Update apple-cards-carousel.tsx

--- src/components/ui/apple-cards-carousel.tsx
+++ src/components/ui/apple-cards-carousel.tsx
@@ -120,8 +120,7 @@ export const Carousel = ({ items, initialScroll = 0 }: CarouselProps) => {
                   transition: {
                     duration: 0.5,
                     delay: 0.2 * index,
-                    ease: "easeOut",
-                    once: true,
+                    ease: "easeOut"
                   },
                 }}
                 key={"card" + index}
